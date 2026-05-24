5435a74d97c01bf909493ada5ca5b0090b809690
Dublonx <dublonjmx17@gmail.com>
2026-05-24 16:02:18 +0800

    Update everything

--- main.js
+++ main.js
@@ -4,4 +4,22 @@ const nav = document.querySelector('#nav');
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("active");
+
+    // changed icon
+    if (nav.classList.contains("active")) {
+        menu.className = "fa-solid fa-xmark";
+    } else {
+        menu.className = "fa-solid fa-bars";
+    }
+});
+
+// close navbar
+const links = document.querySelectorAll('.link');
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+
+        menu.className = "fa-solid fa-bars";
+    });
 });
