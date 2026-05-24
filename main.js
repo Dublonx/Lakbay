bcb71222877725f569bcb8241ced23b77d829dc9
Dublonx <dublonjmx17@gmail.com>
2026-05-24 15:14:26 +0800

    Added functionality for menu bar

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // Lakbay
+const menu = document.querySelector('#menu');
+const nav = document.querySelector('#nav');
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
